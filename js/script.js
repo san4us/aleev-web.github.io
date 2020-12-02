792d69eaaf57b20a693af1758328087392021e96
Timur <san4us2102@gmail.com>
2020-12-02 18:19:56 +0300

    up2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 let btnDefault = document.querySelector('.btn-default');
+let intro = document.querySelector('.intro');
 
-btnDefault.addEventListener('click', () => { alert('Щелчок по кнопке!'); });
+btnDefault.addEventListener('click', () => { alert('Клик по кнопке!'); });
+
+intro.style.border = '5px dashed gold';
+document.body.style.backgroundColor = 'lightgray';
